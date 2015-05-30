cf1122441c60938525e935ebedfdf5194e7cdca4
SamLangTen <samlangten@outlook.com>
2015-05-31 00:09:46 +0800

    add karaoke show function

--- KaraokeShow/Windows/Display/Displays/SampleDisplayForm.Designer.vb
+++ KaraokeShow/Windows/Display/Displays/SampleDisplayForm.Designer.vb
@@ -22,58 +22,18 @@ Partial Class SampleDisplayForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.Label3 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Microsoft YaHei", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
-        Me.Label1.Location = New System.Drawing.Point(12, 9)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(118, 41)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Label1"
-        '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.Font = New System.Drawing.Font("Microsoft YaHei", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
-        Me.Label2.Location = New System.Drawing.Point(16, 105)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(59, 21)
-        Me.Label2.TabIndex = 1
-        Me.Label2.Text = "Label2"
-        '
-        'Label3
-        '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(129, 109)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(46, 17)
-        Me.Label3.TabIndex = 2
-        Me.Label3.Text = "Label3"
         '
         'SampleDisplayForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 17.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(763, 207)
-        Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.Label1)
         Me.Font = New System.Drawing.Font("Microsoft YaHei", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
         Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Name = "SampleDisplayForm"
         Me.Text = "LyricForm"
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
-
-    Friend WithEvents Label1 As Windows.Forms.Label
-    Friend WithEvents Label2 As Windows.Forms.Label
-    Friend WithEvents Label3 As Windows.Forms.Label
 End Class
--- KaraokeShow/Windows/Display/Displays/SampleDisplayForm.vb
+++ KaraokeShow/Windows/Display/Displays/SampleDisplayForm.vb
@@ -1,19 +1,64 @@
 ﻿Imports System.Windows.Forms
+Imports System.Drawing
+Imports System.Drawing.Drawing2D
 Imports System.Threading.Tasks
 Imports System.Threading
 Imports MusicBeePlugin.Plugin
 Imports System.IO
 
 Public Class SampleDisplayForm
+    Private _lyric As String
+    Private _percentage As Double
+
+    Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        Me.SetStyle(ControlStyles.AllPaintingInWmPaint, True)
+        Me.SetStyle(ControlStyles.DoubleBuffer, True)
+        Me.SetStyle(ControlStyles.OptimizedDoubleBuffer, True)
+        Me.SetStyle(ControlStyles.ResizeRedraw, True)
+        Me.SetStyle(ControlStyles.Selectable, True)
+        Me.SetStyle(ControlStyles.SupportsTransparentBackColor, True)
+        Me.SetStyle(ControlStyles.UserPaint, True)
+        Me.UpdateStyles()
+
+    End Sub
 
     Public WriteOnly Property Lyric As String
         Set(value As String)
-            Me.Label1.Text = value
+            _lyric = value
+            Me.Refresh()
         End Set
     End Property
     Public WriteOnly Property Percentage As Double
         Set(value As Double)
-            Me.Label3.Text = (value * 100).ToString() + "%"
+            _percentage = value
+            Me.Refresh()
         End Set
     End Property
+
+    Protected Overrides Sub OnPaint(e As PaintEventArgs)
+        MyBase.OnPaint(e)
+        e.Graphics.CompositingQuality = CompositingQuality.HighSpeed
+        e.Graphics.SmoothingMode = SmoothingMode.HighSpeed
+
+        If _lyric Is Nothing Then Exit Sub
+        Dim gp As New GraphicsPath()
+        gp.AddString(_lyric, New FontFamily("微软雅黑"), 0, 50, New Point(10, 10), StringFormat.GenericTypographic)
+
+        Dim regionLeft As New Region(gp)
+        Dim regionRight As New Region(gp)
+        Dim rect As RectangleF = regionLeft.GetBounds(e.Graphics)
+        Dim rectLeft As New RectangleF(rect.Location, New SizeF(rect.Width * _percentage, rect.Height))
+        regionLeft.Intersect(rectLeft)
+        Dim rectRight As New RectangleF(New PointF(rect.X + rectLeft.Width, rect.Y), New SizeF(rect.Width - rectLeft.Width, rect.Height))
+        regionRight.Intersect(rectRight)
+        Dim brushRed As New LinearGradientBrush(New Point(0, 0), New Point(10, 10), Color.Red, Color.OrangeRed)
+        Dim brushYellow As New LinearGradientBrush(New Point(0, 0), New Point(10, 10), Color.Yellow, Color.GreenYellow)
+        e.Graphics.FillRegion(brushRed, regionLeft)
+        e.Graphics.FillRegion(brushYellow, regionRight)
+    End Sub
 End Class
