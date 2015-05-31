051e7998d9a816bb0c15880e4f4272077a1de698
SamLangTen <samlangten@outlook.com>
2015-05-31 09:30:31 +0800

    upgrade karaoke show function

--- KaraokeShow/Windows/Display/Displays/SampleDisplayForm.vb
+++ KaraokeShow/Windows/Display/Displays/SampleDisplayForm.vb
@@ -7,8 +7,10 @@ Imports MusicBeePlugin.Plugin
 Imports System.IO
 
 Public Class SampleDisplayForm
+    Private _paintWhat As String=""
     Private _lyric As String
     Private _percentage As Double
+
 
     Sub New()
 
@@ -23,6 +25,7 @@ Public Class SampleDisplayForm
         Me.SetStyle(ControlStyles.Selectable, True)
         Me.SetStyle(ControlStyles.SupportsTransparentBackColor, True)
         Me.SetStyle(ControlStyles.UserPaint, True)
+
         Me.UpdateStyles()
 
     End Sub
@@ -44,21 +47,23 @@ Public Class SampleDisplayForm
         MyBase.OnPaint(e)
         e.Graphics.CompositingQuality = CompositingQuality.HighSpeed
         e.Graphics.SmoothingMode = SmoothingMode.HighSpeed
-
         If _lyric Is Nothing Then Exit Sub
         Dim gp As New GraphicsPath()
         gp.AddString(_lyric, New FontFamily("微软雅黑"), 0, 50, New Point(10, 10), StringFormat.GenericTypographic)
 
         Dim regionLeft As New Region(gp)
-        Dim regionRight As New Region(gp)
+        'Dim regionRight As New Region(gp)
+        e.Graphics.Clip = regionLeft
         Dim rect As RectangleF = regionLeft.GetBounds(e.Graphics)
-        Dim rectLeft As New RectangleF(rect.Location, New SizeF(rect.Width * _percentage, rect.Height))
-        regionLeft.Intersect(rectLeft)
-        Dim rectRight As New RectangleF(New PointF(rect.X + rectLeft.Width, rect.Y), New SizeF(rect.Width - rectLeft.Width, rect.Height))
-        regionRight.Intersect(rectRight)
-        Dim brushRed As New LinearGradientBrush(New Point(0, 0), New Point(10, 10), Color.Red, Color.OrangeRed)
-        Dim brushYellow As New LinearGradientBrush(New Point(0, 0), New Point(10, 10), Color.Yellow, Color.GreenYellow)
-        e.Graphics.FillRegion(brushRed, regionLeft)
-        e.Graphics.FillRegion(brushYellow, regionRight)
+        'Dim rectLeft As New RectangleF(rect.Location, New SizeF(rect.Width * _percentage, rect.Height))
+        'regionLeft.Intersect(rectLeft)
+        'Dim rectRight As New RectangleF(New PointF(rect.X + rectLeft.Width, rect.Y), New SizeF(rect.Width - rectLeft.Width, rect.Height))
+        'regionRight.Intersect(rectRight)
+        Dim brushRed As New LinearGradientBrush(New Point(0, 0), New Point(30, 30), Color.Red, Color.OrangeRed)
+        Dim brushYellow As New LinearGradientBrush(New Point(0, 0), New Point(30, 30), Color.Yellow, Color.GreenYellow)
+        'e.Graphics.FillRegion(brushRed, regionLeft)
+        'e.Graphics.FillRegion(brushYellow, regionRight)
+        e.Graphics.FillRectangle(brushRed, rect.X, rect.Y, Convert.ToInt32(rect.Width * _percentage), rect.Height)
+        e.Graphics.FillRectangle(brushYellow, rect.X + Convert.ToInt32(rect.Width * _percentage), rect.Y, rect.Width - Convert.ToInt32(rect.Width * _percentage), rect.Height)
     End Sub
 End Class
