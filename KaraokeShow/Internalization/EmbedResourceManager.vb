0741dc35cd06886de2d5ed1e8f58c96a11f0befb
SamLangTen <samlangten@outlook.com>
2019-10-21 10:26:52 +0800

    try to upgrade embedresourcemanager to remove dedicated code

--- KaraokeShow/Internalization/EmbedResourceManager.vb
+++ KaraokeShow/Internalization/EmbedResourceManager.vb
@@ -9,13 +9,14 @@ Public Class EmbedResourceManager
     Private _contextTypeInfo As Type
     Private _neutralResourcesCulture As CultureInfo
 
+
     Sub New(t As Type)
         MyBase.New(t)
         _contextTypeInfo = t
     End Sub
 
     Protected Overrides Function InternalGetResourceSet(culture As CultureInfo, createIfNotExists As Boolean, tryParents As Boolean) As ResourceSet
-        Dim rs As ResourceSet = CType(Me.ResourceSets(culture), ResourceSet)
+        Dim rs As ResourceSet = MyBase.InternalGetResourceSet(culture, createIfNotExists, tryParents)
         If rs Is Nothing Then
             Dim store As Stream = Nothing
             Dim resourceFilename As String = Nothing
@@ -27,24 +28,11 @@ Public Class EmbedResourceManager
             store = Me.MainAssembly.GetManifestResourceStream(Me._contextTypeInfo, resourceFilename)
             If store IsNot Nothing Then
                 rs = New ResourceSet(store)
-                AddResourceSet(Me.ResourceSets, culture, rs)
             Else
                 rs = MyBase.InternalGetResourceSet(culture, createIfNotExists, tryParents)
             End If
         End If
         Return rs
     End Function
-    Private Shared Sub AddResourceSet(localResourcesSets As Hashtable, culture As CultureInfo, ByRef rs As ResourceSet)
-        SyncLock localResourcesSets
-            Dim objA As ResourceSet = CType(localResourcesSets(culture), ResourceSet)
-            If objA IsNot Nothing Then
-                If Not Object.Equals(objA, rs) Then
-                    rs.Dispose()
-                    rs = objA
-                End If
-            Else
-                localResourcesSets.Add(culture, rs)
-            End If
-        End SyncLock
-    End Sub
+
 End Class
