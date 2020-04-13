976b3ea3d72f201c545cc027a3029380fe4de766
SamLangTen <samlangten@outlook.com>
2020-04-13 21:44:05 +0800

    add win32 function

---
+++ KaraokeShow/Window/Helper/LayeredWindowWin32.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace MusicBeePlugin.Window.Helper
+{
+
+    public static class LayeredWindowWin32
+    {
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct Point
+        {
+            public int x;
+            public int y;
+            public Point(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct Size
+        {
+            public int cx;
+            public int cy;
+            public Size(int cx, int cy)
+            {
+                this.cx = cx;
+                this.cy = cy;
+            }
+        }
+
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        public struct ARGB
+        {
+            public byte Blue;
+            public byte Green;
+            public byte Red;
+            public byte Alpha;
+        }
+
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        public struct BLENDFUNCTION
+        {
+            public byte BlendOp;
+            public byte BlendFlags;
+            public byte SourceConstantAlpha;
+            public byte AlphaFormat;
+        }
+
+        public const int ULW_COLORKEY = 0x1;
+        public const int ULW_ALPHA = 0x2;
+        public const int ULW_OPAQUE = 0x4;
+        public const byte AC_SRC_OVER = 0x0;
+        public const byte AC_SRC_ALPHA = 0x1;
+        public const int GWL_EXSTYLE = -20;
+        public const int WS_EX_LAYERED = 0x80000;
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowLongA")]
+        public static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("user32.dll")]
+        public static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll")]
+        public static extern bool UpdateLayeredWindow(IntPtr hWnd, IntPtr hdcDst, Point pptDst, Size psize, IntPtr hdcSrc, Point pptSrc, int crKey, BLENDFUNCTION pblend, int dwFlags);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetDC(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        public static extern int ReleaseDC(IntPtr hWnd, IntPtr hDC);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr CreateCompatibleDC(IntPtr hDC);
+
+        [DllImport("user32.dll")]
+        public static extern bool DeleteDC(IntPtr hDC);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SelectObject(IntPtr hDC, IntPtr hObject);
+
+        [DllImport("user32.dll")]
+        public static extern bool DeleteObject(IntPtr hObject);
+
+    }
+}
