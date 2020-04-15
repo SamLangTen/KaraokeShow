8da95271b789256b2a79d1750aa88c47bb8dfb43
SamLangTen <samlangten@outlook.com>
2020-04-15 22:48:03 +0800

    edit native methods name

--- KaraokeShow/Window/FormLyrics.cs
+++ KaraokeShow/Window/FormLyrics.cs
@@ -55,25 +55,25 @@ namespace MusicBeePlugin.Window
 
         public void UpdateLayeredWindowLow(Bitmap bmp)
         {
-            var screenDC = LayeredWindowWin32.GetDC(IntPtr.Zero);
+            var screenDC = NativeMethods.GetDC(IntPtr.Zero);
             var hBitmap = IntPtr.Zero;
-            var memDC = LayeredWindowWin32.CreateCompatibleDC(screenDC);
+            var memDC = NativeMethods.CreateCompatibleDC(screenDC);
             var oldBits = IntPtr.Zero;
             try
             {
-                var topLoc = new LayeredWindowWin32.Point(Left, Top);
-                var bitmapSize = new LayeredWindowWin32.Size(bmp.Width, bmp.Height);
-                var srcLoc = new LayeredWindowWin32.Point(0, 0);
+                var topLoc = new NativeMethods.Point(Left, Top);
+                var bitmapSize = new NativeMethods.Size(bmp.Width, bmp.Height);
+                var srcLoc = new NativeMethods.Point(0, 0);
                 hBitmap = bmp.GetHbitmap(Color.FromArgb(0));
-                oldBits = LayeredWindowWin32.SelectObject(memDC, hBitmap);
-                var bfunc = new LayeredWindowWin32.BLENDFUNCTION()
+                oldBits = NativeMethods.SelectObject(memDC, hBitmap);
+                var bfunc = new NativeMethods.BLENDFUNCTION()
                 {
-                    BlendOp = LayeredWindowWin32.AC_SRC_OVER,
-                    AlphaFormat = LayeredWindowWin32.AC_SRC_ALPHA,
+                    BlendOp = NativeMethods.AC_SRC_OVER,
+                    AlphaFormat = NativeMethods.AC_SRC_ALPHA,
                     SourceConstantAlpha = 255,
                     BlendFlags = 0
                 };
-                LayeredWindowWin32.UpdateLayeredWindow(Handle, screenDC, ref topLoc, ref bitmapSize, memDC, ref srcLoc, Color.FromArgb(255, 255, 255).ToArgb(), ref bfunc, LayeredWindowWin32.ULW_ALPHA);
+                NativeMethods.UpdateLayeredWindow(Handle, screenDC, ref topLoc, ref bitmapSize, memDC, ref srcLoc, Color.FromArgb(255, 255, 255).ToArgb(), ref bfunc, NativeMethods.ULW_ALPHA);
             }
             catch (Exception)
             {
@@ -84,11 +84,11 @@ namespace MusicBeePlugin.Window
             {
                 if (hBitmap != IntPtr.Zero)
                 {
-                    LayeredWindowWin32.SelectObject(memDC, oldBits);
-                    LayeredWindowWin32.DeleteObject(hBitmap);
+                    NativeMethods.SelectObject(memDC, oldBits);
+                    NativeMethods.DeleteObject(hBitmap);
                 }
-                LayeredWindowWin32.ReleaseDC(IntPtr.Zero, screenDC);
-                LayeredWindowWin32.DeleteDC(memDC);
+                NativeMethods.ReleaseDC(IntPtr.Zero, screenDC);
+                NativeMethods.DeleteDC(memDC);
             }
             bmp.Dispose();
         }
@@ -112,7 +112,7 @@ namespace MusicBeePlugin.Window
 
         private void FormLyrics_Load(object sender, EventArgs e)
         {
-            LayeredWindowWin32.SetWindowLong(Handle, LayeredWindowWin32.GWL_EXSTYLE, LayeredWindowWin32.GetWindowLong(Handle, LayeredWindowWin32.GWL_EXSTYLE) | LayeredWindowWin32.WS_EX_LAYERED);
+            NativeMethods.SetWindowLong(Handle, NativeMethods.GWL_EXSTYLE, NativeMethods.GetWindowLong(Handle, NativeMethods.GWL_EXSTYLE) | NativeMethods.WS_EX_LAYERED);
         }
 
         #region "For Move & Resize"
